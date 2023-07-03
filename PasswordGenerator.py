1429d9d9134276e7876afed59d9b00064724c5f1
Mohit Patil <93429104+mohiittt@users.noreply.github.com>
2023-07-03 12:45:58 +0530

    PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -10,3 +10,5 @@ if __name__ == "__main__":
     password_length = int(input("Enter the desired password length: "))
     password = generate_password(password_length)
     print("Generated Password:", password)
+
+"""string.ascii_letters (containing both uppercase and lowercase letters), string.digits (containing digits 0-9), and string.punctuation (containing special characters) to create a password of the specified length."""
